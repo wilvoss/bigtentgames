cc4a9cf1462c1aedbf53951213de4af18647cba2
wilvoss <wilv@allovus.com>
2023-09-06 11:57:34 -0700

    likeme 4.2.088 - sw always loads .txt files from server

--- models/PageObject.js
+++ models/PageObject.js
@@ -58,6 +58,11 @@ var Pages = [
     versionHistory:
       '<h2>Version History</h2>' +
       '             <ul>' +
+      '               <li>4.2.088</li>' +
+      '               <ul>' +
+      '                 <li>sw.js now always loads .txt files from server</li>' +
+      '               </ul>' +
+      '' +
       '               <li>4.2.087</li>' +
       '               <ul>' +
       '                 <li>daily challenge bug fixes</li>' +
